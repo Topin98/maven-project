9c43bd3444b202ab4ec1585755d2821b8955d73a
Topin98 <marielina98@hotmail.com>
2019-06-16 14:23:36 +0200

    Solucion errores de sonar

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,10 +9,9 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-
+    // este metodo puede estar vacio
   }
 
-  //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
